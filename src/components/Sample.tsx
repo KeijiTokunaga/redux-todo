a954b9b0e32d70c45fe4d72ccc990c68cdbf6b87
Keiji Tokunaga <keizi81@gmail.com>
2021-08-30 22:08:23 +0900

    不要ファイル削除

---
+++ src/components/Sample.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Sample() {
+  return (
+    <div>
+      <button className="button" onClick={() => console.log("button")}>
+        ボタン
+      </button>
+    </div>
+  );
+}
+
+export default Sample;
