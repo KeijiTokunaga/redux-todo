14af20a07d0e8e01e536d2c5e2a1d719d171317d
Keiji Tokunaga <keizi81@gmail.com>
2021-08-31 00:31:30 +0900

    TaskFromの追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Header from "./components/header/Header";
+import styles from "./App.module.scss";
+import TaskForm from "./features/task/taskForm/TaskForm";
 
-const App = () => {
-  return <div>App</div>;
+const App: React.FC = () => {
+  return (
+    <div className={styles.root}>
+      <div className={styles.wrapper}>
+        <Header />
+        <TaskForm />
+      </div>
+    </div>
+  );
 };
 
 export default App;
